9398fb418a667fb4ae19a750aa5e8fb8765925eb
Gustavo Haddad <gustavoeliashaddad@gmail.com>
2024-06-13 19:51:58 -0300

    refactor: nova cor de fundo das telas e retirada das bordas dos campos de entrada

--- TolyID/MVVM/ViewModels/CadastroCapturaViewModel.cs
+++ TolyID/MVVM/ViewModels/CadastroCapturaViewModel.cs
@@ -61,64 +61,64 @@ public partial class CadastroCapturaViewModel
     {
         DadosGerais = new List<CampoCadastroModel>
         {
-            new CampoCadastroModel("Nº IDENTIFICAÇÃO", CriaEntryComTecladoNumerico(Captura.DadosGerais, nameof(Captura.DadosGerais.NumeroIdentificacao))),
-            new CampoCadastroModel("LOCAL DE CAPTURA", CriaEntryComTecladoNormal(Captura.DadosGerais, nameof(Captura.DadosGerais.LocalDeCaptura))),
-            new CampoCadastroModel("EQUIPE RESPONSÁVEL", CriaEntryComTecladoNormal(Captura.DadosGerais, nameof(Captura.DadosGerais.EquipeResponsavel))),
-            new CampoCadastroModel("INSTITUIÇÃO", CriaEntryComTecladoNormal(Captura.DadosGerais, nameof(Captura.DadosGerais.Instituicao))),
-            new CampoCadastroModel("PESO", CriaEntryComTecladoNumerico(Captura.DadosGerais, nameof(Captura.DadosGerais.Peso))),
-            new CampoCadastroModel("DATA DE CAPTURA", CriaDatePicker(Captura.DadosGerais, nameof(Captura.DadosGerais.DataDeCaptura))),
-            new CampoCadastroModel("HORÁRIO DE CAPTURA", CriaTimePicker(Captura.DadosGerais, nameof(Captura.DadosGerais.HorarioDeCaptura))),
-            new CampoCadastroModel("CONTATO DO RESPONSÁVEL", CriaEntryComTecladoNormal(Captura.DadosGerais, nameof(Captura.DadosGerais.ContatoDoResponsavel))),
-            new CampoCadastroModel("OBSERVAÇÕES", CriaEditor(Captura.DadosGerais, nameof(Captura.DadosGerais.Observacoes)))
+            new CampoCadastroModel("Nº Identificação", CriaEntryComTecladoNumerico(Captura.DadosGerais, nameof(Captura.DadosGerais.NumeroIdentificacao))),
+            new CampoCadastroModel("Local De Captura", CriaEntryComTecladoNormal(Captura.DadosGerais, nameof(Captura.DadosGerais.LocalDeCaptura))),
+            new CampoCadastroModel("Equipe Responsável", CriaEntryComTecladoNormal(Captura.DadosGerais, nameof(Captura.DadosGerais.EquipeResponsavel))),
+            new CampoCadastroModel("Instituição", CriaEntryComTecladoNormal(Captura.DadosGerais, nameof(Captura.DadosGerais.Instituicao))),
+            new CampoCadastroModel("Peso", CriaEntryComTecladoNumerico(Captura.DadosGerais, nameof(Captura.DadosGerais.Peso))),
+            new CampoCadastroModel("Data De Captura", CriaDatePicker(Captura.DadosGerais, nameof(Captura.DadosGerais.DataDeCaptura))),
+            new CampoCadastroModel("Horário De Captura", CriaTimePicker(Captura.DadosGerais, nameof(Captura.DadosGerais.HorarioDeCaptura))),
+            new CampoCadastroModel("Contato do Responsável", CriaEntryComTecladoNormal(Captura.DadosGerais, nameof(Captura.DadosGerais.ContatoDoResponsavel))),
+            new CampoCadastroModel("Observações", CriaEditor(Captura.DadosGerais, nameof(Captura.DadosGerais.Observacoes)))
         };
 
         FichaAnestesica = new List<CampoCadastroModel>
         {
-            new CampoCadastroModel("TIPO DE ANESTÉSICO/DOSE", CriaEntryComTecladoNormal(Captura.FichaAnestesica, nameof(Captura.FichaAnestesica.TipoAnestesicoOuDose))),
-            new CampoCadastroModel("VIA DE ADMINISTRAÇÃO", CriaEntryComTecladoNormal(Captura.FichaAnestesica, nameof(Captura.FichaAnestesica.ViaDeAdministracao))),
-            new CampoCadastroModel("APLICAÇÃO", CriaTimePicker(Captura.FichaAnestesica, nameof(Captura.FichaAnestesica.Aplicacao))),
-            new CampoCadastroModel("INDUÇÃO", CriaTimePicker(Captura.FichaAnestesica, nameof(Captura.FichaAnestesica.Inducao))),
-            new CampoCadastroModel("RETORNO", CriaTimePicker(Captura.FichaAnestesica, nameof(Captura.FichaAnestesica.Retorno)))
+            new CampoCadastroModel("Tipo De Anestésico/Dose", CriaEntryComTecladoNormal(Captura.FichaAnestesica, nameof(Captura.FichaAnestesica.TipoAnestesicoOuDose))),
+            new CampoCadastroModel("Via De Administração", CriaEntryComTecladoNormal(Captura.FichaAnestesica, nameof(Captura.FichaAnestesica.ViaDeAdministracao))),
+            new CampoCadastroModel("Aplicação", CriaTimePicker(Captura.FichaAnestesica, nameof(Captura.FichaAnestesica.Aplicacao))),
+            new CampoCadastroModel("Indução", CriaTimePicker(Captura.FichaAnestesica, nameof(Captura.FichaAnestesica.Inducao))),
+            new CampoCadastroModel("Retorno", CriaTimePicker(Captura.FichaAnestesica, nameof(Captura.FichaAnestesica.Retorno)))
         };
 
         Biometria = new List<CampoCadastroModel>
         {
-            new CampoCadastroModel("COMPRIMENTO TOTAL", CriaEntryComTecladoNumerico(Captura.Biometria, nameof(Captura.Biometria.ComprimentoTotal))),
-            new CampoCadastroModel("COMPRIMENTO DA CABEÇA", CriaEntryComTecladoNumerico(Captura.Biometria, nameof(Captura.Biometria.ComprimentoDaCabeca))),
-            new CampoCadastroModel("LARGURA DA CABEÇA", CriaEntryComTecladoNumerico(Captura.Biometria, nameof(Captura.Biometria.LarguraDaCabeca))),
-            new CampoCadastroModel("PADRÃO ESCUDO CEFÁLICO", CriaEntryComTecladoNormal(Captura.Biometria, nameof(Captura.Biometria.PadraoEscudoCefalico))),
-            new CampoCadastroModel("COMPRIMENTO ESCUDO CEFÁLICO", CriaEntryComTecladoNumerico(Captura.Biometria, nameof(Captura.Biometria.ComprimentoEscudoCefalico))),
-            new CampoCadastroModel("LARGURA ESCUDO CEFÁLICO", CriaEntryComTecladoNumerico(Captura.Biometria, nameof(Captura.Biometria.LarguraEscudoCefalico))),
-            new CampoCadastroModel("LARGURA INTER-ORBITAL", CriaEntryComTecladoNumerico(Captura.Biometria, nameof(Captura.Biometria.LarguraInterOrbital))),
-            new CampoCadastroModel("COMPRIMENTO DA ORELHA", CriaEntryComTecladoNumerico(Captura.Biometria, nameof(Captura.Biometria.ComprimentoDaOrelha))),
-            new CampoCadastroModel("COMPRIMENTO DA CAUDA", CriaEntryComTecladoNumerico(Captura.Biometria, nameof(Captura.Biometria.ComprimentoDaCauda))),
-            new CampoCadastroModel("LARGURA DA CAUDA", CriaEntryComTecladoNumerico(Captura.Biometria, nameof(Captura.Biometria.LarguraDaCauda))),
-            new CampoCadastroModel("COMPRIMENTO ESCUDO ESCAPULAR", CriaEntryComTecladoNumerico(Captura.Biometria, nameof(Captura.Biometria.ComprimentoEscudoEscapular))),
-            new CampoCadastroModel("SEMICIRCUNFERÊNCIA ESCUDO ESCAPULAR", CriaEntryComTecladoNumerico(Captura.Biometria, nameof(Captura.Biometria.SemicircunferenciaEscudoEscapular))),
-            new CampoCadastroModel("COMPRIMENTO ESCUDO PÉLVICO", CriaEntryComTecladoNumerico(Captura.Biometria, nameof(Captura.Biometria.ComprimentoEscudoPelvico))),
-            new CampoCadastroModel("SEMICIRCUNFERÊNCIA ESCUDO PÉLVICO", CriaEntryComTecladoNumerico(Captura.Biometria, nameof(Captura.Biometria.SemicircunferenciaEscudoPelvico))),
-            new CampoCadastroModel("LARGURA NA 2ª CINTA", CriaEntryComTecladoNumerico(Captura.Biometria, nameof(Captura.Biometria.LarguraNaSegundaCinta))),
-            new CampoCadastroModel("NÚMERO DE CINTAS", CriaEntryComTecladoNumerico(Captura.Biometria, nameof(Captura.Biometria.NumeroDeCintas))),
-            new CampoCadastroModel("COMPRIMENTO MÃO SEM UNHA", CriaEntryComTecladoNumerico(Captura.Biometria, nameof(Captura.Biometria.ComprimentoMaoSemUnha))),
-            new CampoCadastroModel("COMPRIMENTO UNHA DA MÃO", CriaEntryComTecladoNumerico(Captura.Biometria, nameof(Captura.Biometria.ComprimentoUnhaDaMao))),
-            new CampoCadastroModel("COMPRIMENTO PÉ SEM UNHA", CriaEntryComTecladoNumerico(Captura.Biometria, nameof(Captura.Biometria.ComprimentoPeSemUnha))),
-            new CampoCadastroModel("COMPRIMENTO UNHA DO PÉ", CriaEntryComTecladoNumerico(Captura.Biometria, nameof(Captura.Biometria.ComprimentoUnhaDoPe))),
-            new CampoCadastroModel("COMPRIMENTO DO PÊNIS", CriaEntryComTecladoNumerico(Captura.Biometria, nameof(Captura.Biometria.ComprimentoDoPenis))),
-            new CampoCadastroModel("LARGURA BASE DO PÊNIS", CriaEntryComTecladoNumerico(Captura.Biometria, nameof(Captura.Biometria.LarguraBasePenis))),
-            new CampoCadastroModel("COMPRIMENTO DO CLITÓRIS", CriaEntryComTecladoNumerico(Captura.Biometria, nameof(Captura.Biometria.ComprimentoDoClitoris)))
+            new CampoCadastroModel("Comprimento Total", CriaEntryComTecladoNumerico(Captura.Biometria, nameof(Captura.Biometria.ComprimentoTotal))),
+            new CampoCadastroModel("Comprimento Da Cabeça", CriaEntryComTecladoNumerico(Captura.Biometria, nameof(Captura.Biometria.ComprimentoDaCabeca))),
+            new CampoCadastroModel("Largura Da Cabeça", CriaEntryComTecladoNumerico(Captura.Biometria, nameof(Captura.Biometria.LarguraDaCabeca))),
+            new CampoCadastroModel("Padrão Escudo Cefálico", CriaEntryComTecladoNormal(Captura.Biometria, nameof(Captura.Biometria.PadraoEscudoCefalico))),
+            new CampoCadastroModel("Comprimento Escudo Cefálico", CriaEntryComTecladoNumerico(Captura.Biometria, nameof(Captura.Biometria.ComprimentoEscudoCefalico))),
+            new CampoCadastroModel("Largura Escudo Cefálico", CriaEntryComTecladoNumerico(Captura.Biometria, nameof(Captura.Biometria.LarguraEscudoCefalico))),
+            new CampoCadastroModel("Largura Inter-Orbital", CriaEntryComTecladoNumerico(Captura.Biometria, nameof(Captura.Biometria.LarguraInterOrbital))),
+            new CampoCadastroModel("Comprimento Da Orelha", CriaEntryComTecladoNumerico(Captura.Biometria, nameof(Captura.Biometria.ComprimentoDaOrelha))),
+            new CampoCadastroModel("Comprimento Da Cauda", CriaEntryComTecladoNumerico(Captura.Biometria, nameof(Captura.Biometria.ComprimentoDaCauda))),
+            new CampoCadastroModel("Largura Da Cauda", CriaEntryComTecladoNumerico(Captura.Biometria, nameof(Captura.Biometria.LarguraDaCauda))),
+            new CampoCadastroModel("Comprimento Escudo Escapular", CriaEntryComTecladoNumerico(Captura.Biometria, nameof(Captura.Biometria.ComprimentoEscudoEscapular))),
+            new CampoCadastroModel("Semicircunferência Escudo Escapular", CriaEntryComTecladoNumerico(Captura.Biometria, nameof(Captura.Biometria.SemicircunferenciaEscudoEscapular))),
+            new CampoCadastroModel("Comprimento Escudo Pélvico", CriaEntryComTecladoNumerico(Captura.Biometria, nameof(Captura.Biometria.ComprimentoEscudoPelvico))),
+            new CampoCadastroModel("Semicircunferência Escudo Pélvico", CriaEntryComTecladoNumerico(Captura.Biometria, nameof(Captura.Biometria.SemicircunferenciaEscudoPelvico))),
+            new CampoCadastroModel("Largura Na 2ª Cinta", CriaEntryComTecladoNumerico(Captura.Biometria, nameof(Captura.Biometria.LarguraNaSegundaCinta))),
+            new CampoCadastroModel("Número De Cintas", CriaEntryComTecladoNumerico(Captura.Biometria, nameof(Captura.Biometria.NumeroDeCintas))),
+            new CampoCadastroModel("Comprimento Mão Sem Unha", CriaEntryComTecladoNumerico(Captura.Biometria, nameof(Captura.Biometria.ComprimentoMaoSemUnha))),
+            new CampoCadastroModel("Comprimento Unha Da Mão", CriaEntryComTecladoNumerico(Captura.Biometria, nameof(Captura.Biometria.ComprimentoUnhaDaMao))),
+            new CampoCadastroModel("Comprimento Pé Sem Unha", CriaEntryComTecladoNumerico(Captura.Biometria, nameof(Captura.Biometria.ComprimentoPeSemUnha))),
+            new CampoCadastroModel("Comprimento Unha Do Pé", CriaEntryComTecladoNumerico(Captura.Biometria, nameof(Captura.Biometria.ComprimentoUnhaDoPe))),
+            new CampoCadastroModel("Comprimento Do Pênis", CriaEntryComTecladoNumerico(Captura.Biometria, nameof(Captura.Biometria.ComprimentoDoPenis))),
+            new CampoCadastroModel("Largura Base Do Pênis", CriaEntryComTecladoNumerico(Captura.Biometria, nameof(Captura.Biometria.LarguraBasePenis))),
+            new CampoCadastroModel("Comprimento Do Clitóris", CriaEntryComTecladoNumerico(Captura.Biometria, nameof(Captura.Biometria.ComprimentoDoClitoris)))
         };
 
         Amostras = new List<CampoCadastroModel>
         {
-            new CampoCadastroModel("SANGUE", CriaCheckBox(Captura.Amostras, nameof(Captura.Amostras.Sangue))),
-            new CampoCadastroModel("FEZES", CriaCheckBox(Captura.Amostras, nameof(Captura.Amostras.Fezes))),
-            new CampoCadastroModel("PELO", CriaCheckBox(Captura.Amostras, nameof(Captura.Amostras.Pelo))),
-            new CampoCadastroModel("ECTOPARASITOS", CriaCheckBox(Captura.Amostras, nameof(Captura.Amostras.Ectoparasitos))),
-            new CampoCadastroModel("SWAB", CriaCheckBox(Captura.Amostras, nameof(Captura.Amostras.Swab))),
-            new CampoCadastroModel("LOCAL", CriaCheckBox(Captura.Amostras, nameof(Captura.Amostras.Local)))
+            new CampoCadastroModel("Sangue", CriaCheckBox(Captura.Amostras, nameof(Captura.Amostras.Sangue))),
+            new CampoCadastroModel("Fezes", CriaCheckBox(Captura.Amostras, nameof(Captura.Amostras.Fezes))),
+            new CampoCadastroModel("Pelo", CriaCheckBox(Captura.Amostras, nameof(Captura.Amostras.Pelo))),
+            new CampoCadastroModel("Ectoparasitos", CriaCheckBox(Captura.Amostras, nameof(Captura.Amostras.Ectoparasitos))),
+            new CampoCadastroModel("Swab", CriaCheckBox(Captura.Amostras, nameof(Captura.Amostras.Swab))),
+            new CampoCadastroModel("Local", CriaCheckBox(Captura.Amostras, nameof(Captura.Amostras.Local)))
         };
 
-        Outros = new("OUTROS", CriaEditor(Captura.Amostras, nameof(Captura.Amostras.Outros)));
+        Outros = new("Outros", CriaEditor(Captura.Amostras, nameof(Captura.Amostras.Outros)));
         ParametrosFisiologicos = new();
     }
 
