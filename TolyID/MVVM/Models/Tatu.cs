0de58af251f4ee5e84149631b2b617d7301ae944
Gellyorge <gellyorge23@gmail.com>
2024-10-22 11:17:46 -0300

    adicao da estrutura basica de comunicacao com api

--- TolyID/MVVM/Models/Tatu.cs
+++ TolyID/MVVM/Models/Tatu.cs
@@ -1,17 +1,27 @@
 ﻿using CommunityToolkit.Mvvm.ComponentModel;
+using Newtonsoft.Json;
 using SQLite;
 using SQLiteNetExtensions.Attributes;
 
 namespace TolyID.MVVM.Models;
 
-[Table("Tatus")]
-public class Tatu : ObservableObject
+[Table("Tatu")]
+public class Tatu
 {
     [PrimaryKey, AutoIncrement]
     public int Id { get; set; }
+
+    [JsonProperty("identificacaoAnimal")]
     public string? IdentificacaoAnimal { get; set; }
+
+    [JsonProperty("numeroMicrochip")]
     public int NumeroMicrochip { get; set; }
 
     [OneToMany(CascadeOperations = CascadeOperation.All)]
-    public List<Captura> Capturas { get; set; }
+    public List<Captura>? Capturas { get; set; }
+
+    public override string ToString()
+    {
+        return $"Id: {Id}\nIdentificacao do Animal: {IdentificacaoAnimal}\nNumero do Microchip: {NumeroMicrochip}\n";
+    }
 }
---
+++ TolyID/MVVM/Models/Usuario.cs
@@ -0,0 +1,18 @@
+﻿using Newtonsoft.Json;
+using SQLite;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TolyID.MVVM.Models
+{
+    public class Usuario
+    {
+        [JsonProperty("email")]
+        public string? Email { get; set; }
+        [JsonProperty("senha")]
+        public string? Senha { get; set; }
+    }
+}
