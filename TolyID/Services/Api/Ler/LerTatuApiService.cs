a5a22a32970d3e34abef15078125c0b0f9201813
Gellyorge <gellyorge23@gmail.com>
2024-10-25 14:01:48 -0300

    funcao receber tatus da api

---
+++ TolyID/Services/Api/Ler/LerTatuApiService.cs
@@ -0,0 +1,49 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Net.Http.Headers;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using TolyID.MVVM.Models;
+using static System.Net.WebRequestMethods;
+
+namespace TolyID.Services.Api.Ler
+{
+    public class LerTatuApiService:BaseApi
+    {
+        private async Task<List<Tatu>> ReceberTatus(string token)
+        {
+            try
+            {
+                using (HttpClient client = new HttpClient())
+                {
+                    string url = $"http://{UrlBaseApi}:8080/tatus/listar";
+                    client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", token);
+
+                    HttpResponseMessage response = await client.GetAsync(url);
+
+                    if (response.IsSuccessStatusCode)
+                    {
+                        var jsonResponse = await response.Content.ReadAsStringAsync();
+                        List<Tatu> res = JsonConvert.DeserializeObject<List<Tatu>>(jsonResponse);
+                        return res;
+                    }
+                    else
+                    {
+                        return null;
+                    }
+                }
+
+            }
+            catch (Exception ex) 
+            {
+                return null;
+            }
+           
+        }
+    }
+}
